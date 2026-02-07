f80fc0d4c35469f02331f46f59cd29163eeed571
Tipirneni Mohan Chand <83950451+mohanchand201@users.noreply.github.com>
2026-02-07 09:03:42 +0530

    Added mail utility functions

--- HelperFunctions.pl
+++ HelperFunctions.pl
@@ -29,6 +29,42 @@ sub println {
     foreach my $arg (@_) { print _stringify($arg) . "\n"; }
 }
 
+# ----------------------------
+# Mail Utility Functions
+# ----------------------------
+
+sub send_mail {
+        # Function to send an email using the sendmail Unix command
+
+    my ($to, $from, $subject, $message_body) = @_;
+
+    # Path to the sendmail executable; adjust if necessary for your system
+    my $sendmail_path = '/usr/sbin/sendmail';
+
+    # Open a pipe to the sendmail command
+    open(MAIL, "|$sendmail_path -t -i") or die "Cannot open sendmail pipe: $!";
+
+    # Print email headers
+    print MAIL "To: $to\n";
+    print MAIL "From: $from\n";
+    print MAIL "Subject: $subject\n";
+    print MAIL "Content-type: text/plain\n\n"; # Required for plain text body
+
+    # Print email body
+    print MAIL $message_body;
+
+    # Close the pipe
+    close(MAIL) or die "Error closing sendmail pipe: $!";
+
+    print "Email sent successfully to $to\n";
+}
+
+my $MAIL_FROM = 'from@***';
+my $MAIL_TO = 'to@***';
+send_mail($MAIL_TO,$MAIL_FROM,'[Testing] Test mail','Please ignore');
+
+
+
 my @fruits = ('apple', 'banana', 'cherry');
 my %person = (name => 'Alice', age => 30, city => 'Paris');
 my $nested = { colors => ['red', 'green', 'blue'], numbers => [1, 2, 3] };
